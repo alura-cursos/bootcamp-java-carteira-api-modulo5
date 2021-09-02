07c5f56a17892ae8ec5459fccc91d0ff18f2bb14
Rodrigo Ferreira <r.caneppele@gmail.com>
2021-09-02 11:47:44 -0300

    codigos da aula 1

--- src/main/java/br/com/alura/carteira/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/carteira/infra/security/SecurityConfigurations.java
@@ -3,6 +3,7 @@ package br.com.alura.carteira.infra.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -18,6 +19,15 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(service).passwordEncoder(passwordEncoder);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+		.anyRequest().authenticated()
+		.and().formLogin()
+		.and().csrf().disable();
 	}
 
 }
